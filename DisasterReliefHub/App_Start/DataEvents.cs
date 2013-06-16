c0824fb2646382df347196efc390fdbd3b0c9500
Ivan Markov <markov.ivan@gmail.com>
2013-06-15 22:19:33 -0500

    events

---
+++ DisasterReliefHub/App_Start/DataEvents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using DisasterReliefHub.Utilities.Json;
+using DisasterReliefHub.Utilities.Json.Handlers;
+
+using Newtonsoft.Json.Linq;
+
+namespace DisasterReliefHub.App_Start
+{
+    public class DataEvents
+    {
+        public static void Setup()
+        {
+            JsonHttpHandler.RegisterAction("PostPeople", delegate(JObject data)
+                {
+                    var result = new JsonResult();
+
+                    return result;
+                });
+        }
+    }
+}
